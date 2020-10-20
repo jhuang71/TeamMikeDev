78e557c3a7fd9c64448a03c32feabece9f9e76d3
Adrian <akirchner@hawk.iit.edu>
2020-10-20 17:38:39 -0500

    Button at the bottom of frontpage now adds "Hermann Hall" to building table in database

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
 import React from "react";
 import "./App.css";
 import Building from "./Building";
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Main from "./Main"
 import iit from './images/iit.png';
+import TestButton from "./TestButton";
 
 
 function App() {
   return (
     <Router>
       <div className="App">
-      <a href="/"><img src={iit} width="500" alt="iit logo" height="110" class="iitImage"/></a>
+        <a href="/"><img src={iit} width="500" alt="iit logo" height="110" class="iitImage" /></a>
 
         <Switch>
           <Route path="/" exact component={Main} />
           <Route path="/:building" component={Building} />
         </Switch>
+        <TestButton />
       </div>
     </Router>
   );
