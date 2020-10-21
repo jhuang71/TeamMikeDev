9baf306791e2ed52d36c223acc34fc68b4c7bf24
rsydorchuk <rsydorchuk@hawk.iit.edu>
2020-10-20 19:16:25 -0500

    temp linking fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./App.css";
 import Building from "./Building";
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import ReserveForm from "./ReserveForm";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Main from "./Main"
 import iit from './images/iit.png';
 
@@ -10,10 +11,11 @@ function App() {
   return (
     <Router>
       <div className="App">
-      <a href="/"><img src={iit} width="500" alt="iit logo" height="110" class="iitImage"/></a>
+        <a href="/"><img src={iit} width="500" alt="iit logo" height="110" class="iitImage" /></a>
 
         <Switch>
           <Route path="/" exact component={Main} />
+          <Route path="/ReserveForm" exact component={ReserveForm} />
           <Route path="/:building" component={Building} />
         </Switch>
       </div>
--- client/src/ReservePopUp.js
+++ client/src/ReservePopUp.js
@@ -1,4 +1,4 @@
-import {Modal, Button} from 'react-bootstrap';
+import { Modal, Button } from 'react-bootstrap';
 import React from 'react'
 
 export default function ReservePopUp(props) {
@@ -11,16 +11,16 @@ export default function ReservePopUp(props) {
     >
 
       <Modal.Body>
-        <h4 style={{textAlign:'center', margin: '1em'}}>Would you like to reserve this space?</h4>
-        <p style={{marginLeft:'1em', marginRight:'1em', textAlign: 'center'}}>
-          By reserving this space, you're agree to use this space respectively and peacefully.<br></br> You are allowed to use this space for a maximum of two hours. After two hours, 
+        <h4 style={{ textAlign: 'center', margin: '1em' }}>Would you like to reserve this space?</h4>
+        <p style={{ marginLeft: '1em', marginRight: '1em', textAlign: 'center' }}>
+          By reserving this space, you're agree to use this space respectively and peacefully.<br></br> You are allowed to use this space for a maximum of two hours. After two hours,
           you will be automatically check out.
         </p>
       </Modal.Body>
       <Modal.Footer>
-        <Button style={{marginRight: '5em'}} onclick={props.onHide}>Yes, I would like to reserve the space</Button>
-        <Button style={{marginRight: '13em'}} variant="secondary" onClick={props.onHide}>Close</Button>
+        <Button href="/ReserveForm" style={{ marginRight: '5em' }} >Yes, I would like to reserve the space</Button>
+        <Button style={{ marginRight: '13em' }} variant="secondary" onClick={props.onHide}>Close</Button>
       </Modal.Footer>
-    </Modal>
+    </Modal >
   );
 }
