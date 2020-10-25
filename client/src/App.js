7974273f92f8359202562162fc2de2f223810ba3
Jay Huang <jhuang71@hawk.iit.edu>
2020-10-25 16:52:23 -0500

    added google login/logout button

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,103 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Building from "./Building";
 import ReserveForm from "./ReserveForm";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Main from "./Main"
-import iit from './images/iit.png';
+import Main from "./Main";
+import iit from "./images/iit.png";
 import TestButton from "./TestButton";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
 
+const UnAuthView = ({ responseGoogle }) => {
+  // Show Login Button
+  return (
+    <div className="GoogleButton">
+      <GoogleLogin
+        clientId="565992343976-3e8cljlucir10s24us3s667l3ujunk29.apps.googleusercontent.com"
+        buttonText="Sign in with Google"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        isSignedIn={true}
+        cookiePolicy={"single_host_origin"}
+      />
+    </div>
+  );
+};
+
+const AuthView = ({ logout, user }) => {
+  // Show Logout Button
+  return (
+    <div className="GoogleOutButton">
+      <img src={user.image} width="50" height="50" alt="profile img"></img>
+      <strong style={{paddingLeft: "15px", paddingRight: "15px"}}>{user.name}</strong>
+      <GoogleLogout
+        clientId="565992343976-3e8cljlucir10s24us3s667l3ujunk29.apps.googleusercontent.com"
+        buttonText="Logout"
+        onLogoutSuccess={logout}
+      />
+    </div>
+  );
+};
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
+  const [user, setUser] = useState({ name: "", email: "", image: "" });
+
+  const responseGoogle = (res) => {
+    console.log(res);
+    const profile = res.getBasicProfile();
+    setUser({
+      name: profile.getName(),
+      email: profile.getEmail(),
+      image: profile.getImageUrl(),
+    });
+  };
+
+
+  useEffect(() => {
+    if (user.email.length > 0 || user.name.length > 0) setIsAuth(true);
+  }, [user]);
+
+  const logout = () => {
+    setUser({ name: "", email: "", image: "" });
+    setIsAuth(false);
+  };
+
   return (
     <Router>
       <div className="App">
-        <a href="/"><img src={iit} width="500" alt="iit logo" height="110" class="iitImage" /></a>
+        <div>
+          <a href="/">
+            <img
+              src={iit}
+              width="450"
+              alt="iit logo"
+              height="100"
+              className="iitImage"
+            />
+          </a>
+          {isAuth ? (
+            <AuthView logout={logout} user={user} />
+          ) : (
+            <UnAuthView responseGoogle={responseGoogle} />
+          )}
+        </div>
 
         <Switch>
           <Route path="/" exact component={Main} />
-          <Route path="/:building" exact component={Building} />
-          <Route path="/:building/ReserveForm" exact component={ReserveForm} />
+          <Route
+            path="/building/:building"
+            exact
+            component={Building}
+            isAuthed={isAuth}
+          />
+          <Route
+            path="/building/:building/ReserveForm"
+            exact
+            component={ReserveForm}
+          />
+          <Route path="/admin" component={TestButton} />
         </Switch>
-        <TestButton />
       </div>
     </Router>
   );
