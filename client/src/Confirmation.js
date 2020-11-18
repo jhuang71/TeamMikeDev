564a5a3790a11a3caa9613ad4f49b1baed8715e5
cfrancis1 <32426276+cfrancis1@users.noreply.github.com>
2020-11-18 15:27:55 -0600

    Added the images to the confirmation page as well
    changed the way the URL worked on the confirmation page to allow the building images to be put there was well.
    
    did error testing and everything seems to be working as intended. If you find anything weird please let me know.

--- client/src/Confirmation.js
+++ client/src/Confirmation.js
@@ -1,13 +1,54 @@
 import React, {useState, useEffect} from 'react';
 import { Button, Image } from 'react-bootstrap';
 import { useParams } from "react-router-dom";
-import hermann from './img/HH-1620X1080.jpg';
-
+import hh from "./img/HH-1620X1080.jpg";
+import mtcc from "./img/mtcc-1607 x 725.jpg";
+import crown from "./img/crown.jpg";
+import galvin from "./img/galvin.jpg";
+import kaplan from "./img/kaplan.jpg";
+import perlstein from "./img/perlstein.jpg";
+import pritzker from "./img/pritzker.jpg";
+import stuart from "./img/stuart.jpg";
+import tower from "./img/tower.jpg";
+import extra from "./img/extra.jpg";
 import './Main.css';
 
 export default function Confirmation() {
     const id = useParams();
     const space_ID = id.spaceID;
+    function buildingSelect() {
+        if(id.building === "hermann") {
+            return hh;
+        } else if (id.building === "mtcc") {
+            return mtcc;
+        } else if (id.building === "iit_tower") {
+            return tower;
+        } else if (id.building === "pritzker") {
+            return pritzker;
+        } else if (id.building === "galvin") {
+            return galvin;
+        } else if (id.building === "stuart") {
+            return stuart;
+        } else if (id.building === "kaplan") {
+            return kaplan;
+        } else if (id.building === "perlstein") {
+            return perlstein;
+        } else if (id.building === "crown") {
+            return crown;
+        } else {
+            return extra;
+        }
+      };
+    
+    function buildingName() {
+        if(id.building === "Eng") {
+            return "ENGINEERING CENTER";
+        } else if(id.building === "iit_tower") {
+            return "IIT TOWER";
+        } else {
+            return id.building.toUpperCase();
+        }
+    };
     //var times = "";
     const [times, setTimes] = useState([]);
 
@@ -48,8 +89,8 @@ export default function Confirmation() {
     return (
         <div>
             <div className="buildingHeader">
-                <Image style={{ opacity: "0.9" }} src={hermann} width="1000" height="750" fluid />
-                <h1 className="centered">Hermann</h1>
+                <Image style={{ opacity: "0.9" }} src={buildingSelect()} width="1000" height="750" fluid />
+                <h1 className="centered">{buildingName()}</h1>
             </div>
             <h2 class="confirmation">Your Reservation on {month + 1}/{date}/{year} from {startHour}:{startMin} to {endHour}:{endMin} has been Accepted</h2>
             <Button href="/" variant="primary" type="submit" class="confirmButton">Return to Home</Button>
